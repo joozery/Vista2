0f7668a10a217adb733c65f357bd7348edec1393
joozery <136419786+joozery@users.noreply.github.com>
2025-01-21 11:57:30 +0700

    เพิ่มไ��������� apple.svg

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar"; // ดึง Navbar
-import SectionVideo from "../components/Sectionvideo/SectionVideo";
+import SectionVideo from "../components/Sectionvideo/sectionVideo";
 import WhyFundingPips from "../components/Sectionvideo/WhyFundingPips"; // ดึง WhyFundingPips
 
 const Home = () => {
